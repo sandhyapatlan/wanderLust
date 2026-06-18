30825087d3b319ee9ce6493a18b839df8c864316
SANDHYA PATLAN <sandhyapatlan@SANDHYAs-MacBook-Air.local>
2026-06-18 22:49:03 +0530

    fix cloudConfig spelling

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware");
 const listingController = require("../controllers/listings.js");
 const multer = require('multer')
-const { storage } = require("../cloudconfig.js");
+const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
 
 // Index + Create
